7a2bfaad59ee409a0c7c06a4a6478838b2b70f2b
Ada Kravchenko <ada.t.krav@gmail.com>
2023-04-23 15:23:00 +0300

    add User and UserProfile models and views with JWT authentication

---
+++ user/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path, include
+from rest_framework import routers
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
+from user.views import CreateUserView, ManageUserView, UserProfileViewSet
+
+router = routers.DefaultRouter()
+router.register("profiles", UserProfileViewSet)
+urlpatterns = [
+    path("register/", CreateUserView.as_view(), name="create"),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path("me/", ManageUserView.as_view(), name="manage"),
+    path("", include(router.urls)),
+]
+
+app_name = "user"
--- user/views.py
+++ user/views.py
@@ -1,3 +1,59 @@
-from django.shortcuts import render
+from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
-# Create your views here.
+from .models import UserProfile
+from .permissions import IsTheUserOrReadOnly
+from .serializers import (
+    UserSerializer,
+    AuthTokenSerializer,
+    UserProfileListSerializer,
+    UserProfileDetailSerializer,
+)
+
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+    serializer_class = AuthTokenSerializer
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
+
+
+class UserProfileViewSet(
+    viewsets.ModelViewSet,
+):
+    queryset = UserProfile.objects.all()
+    permission_classes = (IsAuthenticated, IsTheUserOrReadOnly)
+
+    def get_queryset(self):
+        queryset = self.queryset
+        username = self.request.query_params.get("username")
+        bio = self.request.query_params.get("bio")
+
+        if username:
+            queryset = queryset.filter(username__icontains=username)
+
+        if bio:
+            queryset = queryset.filter(bio__icontains=bio)
+
+        return queryset.distinct()
+
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            return UserProfileListSerializer
+
+        return UserProfileDetailSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
